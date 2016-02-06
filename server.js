8cbb20b2415b01a4a6206dd53fa2ca057054c1e9
Alyson <emerald.aly@gmail.com>
2016-02-06 13:05:35 -0500

    added github package

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var omdb = require('omdb');
+var github = require('github');
+
 var app = express();
 var PORT = process.env.PORT || 8090;
 
@@ -17,6 +20,24 @@ app.get("/dashboard", function(req, res) {
   res.sendFile(process.cwd() + "/views/dashboard.html")
 });
 
+app.get('/movies/:movieName', function(req, res) {
+  omdb.search(req.params.movieName, function(err, movies) {
+    if(movies.length > 0){
+
+    var firstMovie = movies[0];
+  } else {
+    res.send(404)("not found");
+  }
+
+
+    res.send(JSON.stringify(firstMovie));
+  }
+  });
+});
+
+
+//route that accepts github usernames
+//github ap wrapper
 app.listen(PORT, function() {
   console.log("listening on port %s", PORT);
 });
