334b8295545027e430f5af15636c4a10b48beb8c
Alyson <emerald.aly@gmail.com>
2016-02-06 11:19:26 -0500

    added json file

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8090;
+
+app.use("/js", express.static("/public/js"));
+app.use("/css", express.static("/public/css"));
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() +  "/views/home.html");
+});
+
+app.get("/register", function(req, res) {
+  res.sendFile(process.cwd() + "/views/register.html");
+});
+
+app.get("/dashboard", function(req, res) {
+  res.sendFile(process.cwd() + "/views/dashboard.html")
+});
+
+app.listen(PORT, function() {
+  console.log("listening on port %s", PORT);
+});
